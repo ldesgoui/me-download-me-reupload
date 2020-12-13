9bb7521f8871345f8ef61a2ec29cf62d43e4a5b8
ldesgoui <ldesgoui@gmail.com>
2020-12-13 17:05:40 +0100

    fix up ssl, point tag to docker packages

--- image.nix
+++ image.nix
@@ -4,9 +4,11 @@ let
   me-download-me-reupload = import ./default.nix;
 in
 pkgs.dockerTools.buildLayeredImage {
-  name = "me-download-me-reupload";
+  name = "docker.pkg.github.com/ldesgoui/me-download-me-reupload/me-download-me-reupload";
+  created = "now";
 
   contents = [ me-download-me-reupload ];
 
   config.Cmd = [ "me-download-me-reupload" ];
+  config.Env = [ "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" ];
 }
